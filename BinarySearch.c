c9915941782391c03334accc7a3306a5d21699b7
rohitc154 <rohitmonu154@gmail.com>
2023-11-26 12:48:10 +0530

    Binary Searching

--- BinarySearch.c
+++ BinarySearch.c
@@ -2,9 +2,9 @@
 void binSearch();
 void main()
 {
-    int arr[] = {2, 4, 6, 8, 9, 10};
-    int size = sizeof(arr) / sizeof(int);
-    binSearch(arr, 10, size);
+    int arr[] = {2, 4, 6, 8, 9, 10, 12, 16, 27, 74};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    binSearch(arr, 1, size);
 }
 void binSearch(int arr[], int target, int size)
 {
@@ -22,5 +22,5 @@ void binSearch(int arr[], int target, int size)
         else
             start = mid + 1;
     }
-    printf("Not Found!");
+    printf("Value not found!");
 }
